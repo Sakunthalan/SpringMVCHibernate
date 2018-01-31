916a290081cd4ef2b238d3898076962fb034e922
m-meena <35134459+m-meena@users.noreply.github.com>
2018-01-31 11:20:06 +0530

    Test case modification

--- shopback/src/test/java/com/niit/shop/shopback/testcase/SupplierTest.java
+++ shopback/src/test/java/com/niit/shop/shopback/testcase/SupplierTest.java
@@ -9,60 +9,54 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.niit.shop.shopback.dao.SupplierDao;
 import com.niit.shop.shopback.model.Supplier;
 
-
 public class SupplierTest {
-	
+
 	static Supplier supplier;
 	static SupplierDao supplierdao;
 	static AnnotationConfigApplicationContext context;
-	
+
 	@BeforeClass
-	public static  void init()
-	{
+	public static void init() {
 		context = new AnnotationConfigApplicationContext();
 		context.scan("com.niit.shop.shopback");
-		context.refresh();		
+		context.refresh();
 		supplierdao = (SupplierDao) context.getBean("supplierdao");
 	}
-	
 
 	@Test
-	public void supplierCRUDTest()
-	{
-		//add
+	public void addSupplierTest() {
+		// add
 		supplier = new Supplier();
 		supplier.setName("raja");
 		supplier.setAddress("mdu");
 		supplier.setContactNum("7418803793");
 		assertEquals("Added", true, supplierdao.addSupplier(supplier));
-		
+
 		supplier = new Supplier();
 		supplier.setName("aathi");
 		supplier.setAddress("chn");
 		supplier.setContactNum("7418803792");
 		assertEquals("Added", true, supplierdao.addSupplier(supplier));
-		
-		//update
+	}
+
+	@Test
+	public void listSupplierTest() {
+		// list
+		assertEquals("Successfully displayed", 3, supplierdao.supplierList().size());
+	}
+
+	@Test
+	public void updateSupplierTest() {
+		// update
 		supplier = supplierdao.get(4);
 		supplier.setAddress("madurai");
-		assertEquals("Updated",true, supplierdao.updateSupplier(supplier));
-		
-		//delete
-		supplier = supplierdao.get(5);
-		assertEquals("Deleted",true, supplierdao.deleteSupplier(supplier));
-		
-		//list
-		assertEquals("Successfully displayed",3, supplierdao.supplierList().size());
-
+		assertEquals("Updated", true, supplierdao.updateSupplier(supplier));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void deleteSupplierTest() {
+		// delete
+		supplier = supplierdao.get(5);
+		assertEquals("Deleted", true, supplierdao.deleteSupplier(supplier));
+	}
 }
